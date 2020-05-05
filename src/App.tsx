4b2eb7cb93ccdaa5c2683dcfe44353ceb000bae7
Fakhrullah Padzil <fakhrullah@fajarhac.com>
2020-05-05 17:29:28 +0800

    Add fonts and basic content

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>MASUKWAKTU</div>
+      <div>*</div>
+      <div>00:05:43</div>
+      <div>
+      Sebelum Masuk Waktu SUBUH di SUBANG JAYA^
+      </div>
+      <div>
+      dan kawasan-kawasan yang sama waktu dengannya
+      </div>
+      <div>
+      SUBANG JAYA | PETALING JAYA | GOMBAK | HULU LANGAT | SEPANG |
+      HULU SELANGOR | SHAH ALAM
+      </div>
+      <div>
+        <div>
+          IMSAK
+          5:43 am
+        </div>
+        <div>
+          SUBUH
+          5:53 am
+        </div>
+        <div>
+          SYURUK
+          7:02 am
+        </div>
+        <div>
+          ZOHOR
+          1:13 pm
+        </div>
+        <div>
+          ASAR
+          4:32 pm
+        </div>
+        <div>
+          MAGHRIB
+          7:20 pm
+        </div>
+        <div>
+          ISYAK
+          8:31 pm
+        </div>
+      </div>
+      <div>
+        <div>MASUKWAKTU</div>
+        Looking for sponsor about RM3k per year
+      </div>
     </div>
   );
 }
